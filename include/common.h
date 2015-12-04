236701b86fc6420fc8becfcc5b0726bd50dc8352
Hwankyu Jhun <h.jhun@samsung.com>
2015-12-04 20:57:26 +0900

    Refactoring debug-launchpad
    - sync with tizen_2.4
    - use glib main loop
    - remove unnecessary dependencies (x11, aul, ail)
    
    Change-Id: I79652c1a5415f4f5a9bf0cbb68d4c13e8e8a7c8e
    Signed-off-by: Hwankyu Jhun <h.jhun@samsung.com>

---
+++ include/common.h
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#define _GNU_SOURCE
+#include <unistd.h>
+#include <ctype.h>
+#include <dlog.h>
+#include <bundle.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#ifdef LOG_TAG
+#undef LOG_TAG
+#define LOG_TAG "DEBUG_LAUNCHPAD"
+#endif
+
+#define _E(fmt, arg...) LOGE(fmt, ##arg)
+#define _D(fmt, arg...) LOGD(fmt, ##arg)
+#define _W(fmt, arg...) LOGW(fmt, ##arg)
+
+#define SOCKET_PATH "/run/user"
+#define MAX_LOCAL_BUFSZ 128
+#define AUL_SOCK_MAXBUFF 65535
+
+enum app_cmd {
+	APP_START = 0,
+	APP_OPEN = 1,
+	APP_RESUME = 2,
+	APP_START_RES = 7,
+};
+
+typedef struct _app_pkt_t {
+	int cmd;
+	int len;
+	unsigned char data[1];
+} app_pkt_t;
+
+typedef struct {
+	char *appid;
+	char *app_path;
+	char *original_app_path;
+	char *debug_appid;
+	char *pkg_type;
+	char *hwacc;
+	char *taskmanage;
+	char *comp_type;
+} appinfo_t;
+
+struct ucred;
+
+int _create_server_sock(void);
+app_pkt_t *_recv_pkt_raw(int fd, int *clifd, struct ucred *cr);
+int _send_pkt_raw(int client_fd, app_pkt_t *pkt);
+
+appinfo_t *_appinfo_create(bundle *kb);
+void _appinfo_free(appinfo_t *appinfo);
+void _modify_bundle(bundle *kb, int caller_pid, appinfo_t *appinfo, int cmd);
+
+void _set_env(appinfo_t *app_info, bundle *kb);
+char **_create_argc_argv(bundle *kb, int *margc, const char *app_path);
+
+char *_proc_get_cmdline_bypid(int pid);
+
+#endif /* __COMMON_H__ */
